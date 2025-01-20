3a6b42c45eedc9b07a24b21d872306f881f07d03
harunbd80 <harun708280@gmail.com>
2025-01-20 18:56:52 +0600

    update

--- src/Hook/usePublic.jsx
+++ src/Hook/usePublic.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const publicAxios=axios.create({
-    baseURL:'http://localhost:9000',
+    baseURL:'https://my-server-nine-xi.vercel.app',
     withCredentials:true,
 })
 const usePublic = () => {
